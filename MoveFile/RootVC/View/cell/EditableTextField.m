f7fda1f5ff3d6d0699b8b493bca4aa11e95c33f2
wangkq <908891024@qq.com>
2019-10-25 17:16:59 +0800

    1.2_4 输入框颜色设置: 使用系统自动的文字和背景颜色

--- MoveFile/RootVC/View/cell/EditableTextField.m
+++ MoveFile/RootVC/View/cell/EditableTextField.m
@@ -23,18 +23,42 @@
 
 - (id)initWithFrame:(NSRect)frameRect {
     if (self = [super initWithFrame:frameRect]) {
-        self.delegate = self;
+        //self.delegate        = self;
+        
+        // 使用系统自动的文字和背景颜色
+        self.backgroundColor = [NSColor textBackgroundColor];
+        self.textColor       = [NSColor textColor];
+        
     }
     return self;
 }
 
-- (BOOL)becomeFirstResponder {
-    self.backgroundColor = [NSColor whiteColor];
-    return [super becomeFirstResponder];
-}
+// 识别系统主题颜色
+//    NSString *osxMode = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
+//    if (osxMode) {
+//        self.backgroundColor = [NSColor whiteColor];
+//        self.textColor       = [NSColor blackColor];
+//        // NSLog(@"osxMode : %@",osxMode);  //黑暗模式打印：Dark  非黑暗模式: (null)
+//    } else {
+//        self.backgroundColor = [NSColor grayColor];
+//        self.textColor       = [NSColor whiteColor];
+//    }
 
-- (void)controlTextDidEndEditing:(NSNotification *)obj {
-    self.backgroundColor = [NSColor clearColor];
-}
+//- (BOOL)becomeFirstResponder {
+//    return [super becomeFirstResponder];
+//}
+
+//- (void)controlTextDidEndEditing:(NSNotification *)obj {
+//    NSString *osxMode = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
+//    if (osxMode) {
+//        self.backgroundColor = [NSColor clearColor];
+//        self.textColor       = [NSColor whiteColor];
+//        // NSLog(@"osxMode : %@",osxMode);  //黑暗模式打印：Dark  非黑暗模式: (null)
+//    } else {
+//        self.backgroundColor = [NSColor clearColor];
+//        self.textColor       = [NSColor blackColor];
+//    }
+//
+//}
 
 @end
